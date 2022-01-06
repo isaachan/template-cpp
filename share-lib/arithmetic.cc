f6ae030bba344e1a77c190331edbfb71ea24b4da
Kai Han <kai.han@nio.com>
2022-01-06 11:41:44 +0800

    shared lib

---
+++ share-lib/arithmetic.cc
@@ -0,0 +1,17 @@
+#include "shared/arithmetic.h"
+
+namespace shared {
+
+
+int multi(int a, int b) 
+{
+    return a * b;
+}
+
+int divive(int a, int b)
+{
+    return a / b;
+}
+
+
+} // namespace shared
