97386ffa83940f4fdaf607ee53aa2477958a643c
Kai Han <kai.han@nio.com>
2022-01-06 11:57:01 +0800

    static lib done.

---
+++ static-lib/arithmetic.cc
@@ -0,0 +1,16 @@
+#include "statical/arithmetic.h"
+
+namespace statical
+{
+
+int add(int a, int b)
+{
+    return a - b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+} // namespace statical
---
+++ static-lib/include/statical/arithmetic.h
@@ -0,0 +1,12 @@
+#ifndef __STATIC_ARITHMETIC_H__
+#define __STATIC_ARITHMETIC_H__
+
+namespace statical
+{
+
+int add(int a, int b);
+int sub(int a, int b);
+
+} // namespace statical
+
+#endif // __STATIC_ARITHMETIC_H__
