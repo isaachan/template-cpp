68b7cff8390d394f05b6bb1b592e110d15d8ab3c
isaachan <isaachanstar@gmail.com>
2022-06-09 15:03:17 +0800

    temporely remove dependence to libraries

--- src/app.cc
+++ src/app.cc
@@ -1,14 +1,16 @@
-#include <statical/arithmetic.h>
-#include <shared/arithmetic.h>
+//#include <statical/arithmetic.h>
+//#include <shared/arithmetic.h>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "4+2=" << statical::add(4, 2) << endl;
-    cout << "4-2=" << statical::sub(4, 2) << endl;
-    cout << "4*2=" << shared::multi(4, 2) << endl;
-    cout << "4/2=" << shared::divive(4, 2) << endl;
+    //cout << "4+2=" << statical::add(4, 2) << endl;
+    //cout << "4-2=" << statical::sub(4, 2) << endl;
+    //cout << "4*2=" << shared::multi(4, 2) << endl;
+    //cout << "4/2=" << shared::divive(4, 2) << endl;
+    cout << "ha ha ha ha" << endl;
     return 0;
 }
+
